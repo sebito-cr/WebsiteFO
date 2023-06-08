e59bbc9c0bf0e24633f4a602cda93423bef1a488
Josh <110653@st-maartenscollege.nl>
2023-06-08 22:28:56 +0200

    pagina voor spiderman toegevoegd (werkt niet)

--- WebsiteFO/Controllers/HomeController.cs
+++ WebsiteFO/Controllers/HomeController.cs
@@ -41,6 +41,11 @@ namespace WebsiteFO.Controllers
             return View();
         }
 
+        public IActionResult Spiderman()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
